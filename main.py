2a38482b5d3b13d26091d2d6c0aae054889123cd
Vaibhav <vaibhavchaudhari2896@gmail.com>
2023-01-21 14:22:07 +0530

    First commit

---
+++ main.py
@@ -0,0 +1,52 @@
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+import dill
+import re
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from textblob import TextBlob
+
+# Reading all necessary pickle files
+
+vocab = pickle.load(open('count_vector.pickle', 'rb'))
+Products_labels = pickle.load(open('labels_dict_pandas.pickle', 'rb'))
+tfidf_fit = pickle.load(open('tfidf.pickle', 'rb'))
+model = pickle.load(open('lr_model.pickle', 'rb'))
+clean_complaints_func = pickle.load(open('dill_clean_func.pickle', 'rb'))
+clean_data_function = dill.loads(clean_complaints_func)
+app = FastAPI()
+
+
+class TextIn(BaseModel):
+    text: str
+
+
+class PredictionOut(BaseModel):
+    model_predict: str
+
+
+@app.get('/')
+def home():
+    return 'Welcome to Automatic Ticket Classifier'
+
+
+@app.post('/predict')
+def predict_ticket(data: TextIn):
+    cleaned_complaint_test1 = clean_data_function(data)
+    count_vector_test1 = CountVectorizer(vocabulary=vocab)
+    vectored_data = count_vector_test1.transform([cleaned_complaint_test1])
+    tfidf_test1 = tfidf_fit.transform(vectored_data)
+    predict_test1 = model.predict(tfidf_test1)
+    model_predict = Products_labels[predict_test1[0]]
+    print('Ticket belongs to the Product Category: ')
+    return model_predict
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
+
+
+# uvicorn main:app --reload
